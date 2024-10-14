2c7610c5508b568349b3d170e1e6be9c9dd52f9b
Vishwanath reddy <vishwaeshwar143@gmail.com>
2024-10-14 16:06:18 +0530

    commiting changes

---
+++ install-mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then
+    echo "Please run this command with root access"
+    exit
+fi
+
+yum install mysql -y
+if [ $? - ne 0 ]
+then
+    echo "Installation of mysql is failed"
+    exit
+else
+    echo "Installation is Succesfully completed"
+fi
