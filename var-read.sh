8196056e60eda5735e595459ffee63ed327ffa4b
Vishwanath reddy <vishwaeshwar143@gmail.com>
2024-10-14 15:18:20 +0530

    shell-script

---
+++ var-read.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter your Username"
+
+read -s USERNAME
+
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your Password"
+
+read -s PASSWORD
+
+echo "Password entered is: $PASSWORD"
