b362c5e2edaf7d4091fc4b3767a6cc7b2d40036f
Vishwanath reddy <vishwaeshwar143@gmail.com>
2024-10-14 16:08:34 +0530

    commiting changes

--- install-mysql.sh
+++ install-mysql.sh
@@ -5,15 +5,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0]
 then
     echo "Please run this command with root access"
-    exit
+    exit 1
 fi
 
 yum install mysql -y
 
-if [ $? - ne 0 ]
+if [ $? -ne 0 ]
 then
     echo "Installation of mysql is failed"
-    exit
+    exit 1
 else
     echo "Installation is Succesfully completed"
 fi
