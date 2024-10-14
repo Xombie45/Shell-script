7a1ae7a8b556878ea7c2489f1f61143e9e9088f1
Vishwanath reddy <vishwaeshwar143@gmail.com>
2024-10-14 15:41:59 +0530

    commiting changes

--- conditions.sh
+++ conditions.sh
@@ -5,7 +5,7 @@ NUMBER2=$2
 
 if [ "$NUMBER1 -gt $NUMBER2" ]
 then
-    echo "$NUMBER1 is greater than $NUMBER2"
+    echo ""$NUMBER1" is greater than "$NUMBER2""
 else
-    echo "$NUMBER1 is smaller than $NUMBER2"
+    echo ""$NUMBER2" is smaller than "$NUMBER1""
 fi
