f7322826ea6f66b5bcd8aac6561d782d0404cff0
Vishwanath reddy <vishwaeshwar143@gmail.com>
2024-10-14 15:45:33 +0530

    commiting changes

--- conditions.sh
+++ conditions.sh
@@ -1,11 +1,24 @@
+# #!/bin/bash
+
+# NUMBER1=$1
+# NUMBER2=$2
+
+# if [ "$NUMBER1 -lt $NUMBER2" ]
+# then
+#     echo "$NUMBER1" is greater than "$NUMBER2"
+# else
+#     echo "$NUMBER2" is greater than "$NUMBER1"
+# fi
+############
+
 #!/bin/bash
 
-NUMBER1=$1
-NUMBER2=$2
+# check if the number is greater than 10 or not
+NUMBER=$1
 
-if [ "$NUMBER1 -lt $NUMBER2" ]
+if [ $NUMBER -gt 10 ]
 then
-    echo "$NUMBER1" is greater than "$NUMBER2"
+    echo "$NUMBER is greater than 10"
 else
-    echo "$NUMBER2" is greater than "$NUMBER1"
+    echo "$NUMBER is not greater than 10"
 fi
