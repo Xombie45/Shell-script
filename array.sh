f32df8e4632a4d19cba713e728e98f2b3e8bb00b
Vishwanath reddy <vishwaeshwar143@gmail.com>
2024-10-14 15:23:29 +0530

    commiting changes

---
+++ array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this is called array, instead of single value it can hold multiple values
+PERSONS=("Vishwa" "Cahalapay" "Xombie")
+# inside array we always refer values with index and it starts from 0
+
+echo "First Person: ${PERSONS[0]}"
+
+echo "All Persons: ${PERSONS[@]}"
