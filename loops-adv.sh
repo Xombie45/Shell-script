771945da426b7d62f9fd3494798e1d0d9b5a37f0
Vishwanath reddy <vishwaeshwar143@gmail.com>
2024-10-16 16:00:22 +0530

    commiting

--- loops-adv.sh
+++ loops-adv.sh
@@ -3,21 +3,21 @@
 DATE=$(date)
 LOGSDIR=/home/centos/shell-script-logs
 SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ];
 then
     echo -e "$R ERROR :: Please run with root access $N"
     exit 1
 fi
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
+    if [ $1 -ne 0 ];
     then
         echo -e "$R Installing $2 ....... FAILURE $N"
         exit 1
@@ -28,11 +28,11 @@ VALIDATE(){
 
 for i in $@
 do
-    yum list installed $i &>>LOGFILE
+    yum list installed $i &>>$LOGFILE
     if [ $? -ne 0 ]
     then
         echo -e "$Y $i is not installed $N . $G Lets Install it $N"
-        yum install $i -y &>>LOGFILE
+        yum install $i -y &>>$LOGFILE
         VALIDATE $? "$i"
     else
         echo -e "$Y $i is already installed $N"
