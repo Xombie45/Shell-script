d07aaa2ea555b00d7a8b8c54d796b237287a1e8d
Vishwanath reddy <vishwaeshwar143@gmail.com>
2024-10-16 16:32:35 +0530

    commiting

--- delete-logs.sh
+++ delete-logs.sh
@@ -6,7 +6,7 @@ DATE=$(date)
 LOGSDIR=/home/centos/shell-script-logs
 # /home/centos/shell-script-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGSDIR/$0-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
